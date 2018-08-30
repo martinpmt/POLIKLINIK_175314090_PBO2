fe75e175dacf11ca823cac9af537c85f9d8da46d
Martin Paramarta <Martin Paramarta@MARTINPMT>
2018-08-31 04:07:11 +0700

    175314090_PBO2_USD_20181_POLIKLINIK_1.1 Dengan Try Catch Exception

---
+++ src/Poliklinik/Dokter.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Poliklinik;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Martin Paramarta
+ */
+public class Dokter {
+    private String noPegawai;
+    private String nama;
+    private String alamat;
+    private String tempatLahir;
+    private int tanggalLahir;
+    private int bulanLahir;
+    private int tahunLahir;
+
+    public Dokter() {
+
+    }
+
+    public Dokter(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNoPegawai() {
+        return noPegawai;
+    }
+
+    public void setNoPegawai(String noPegawai) throws Exception {
+        if (noPegawai.length() >= 6 && noPegawai.length() <= 20) {
+            this.noPegawai = noPegawai;
+        } else {
+            throw new Exception("No Rekam Medis Salah");
+        }
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public int getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(int tanggalLahir) throws Exception {
+        if (tanggalLahir > 0) {
+            if (tanggalLahir < 32) {
+                this.tanggalLahir = tanggalLahir;
+            } else {
+                throw new Exception("Tanggal Salah ===> Lebih dari 31");
+            }
+        } else {
+            throw new Exception("Tanggal Salah ===> Kurang dari 0");
+        }
+    }
+
+    public int getBulanLahir() {
+        return bulanLahir;
+    }
+
+    public void setBulanLahir(int bulanLahir) throws Exception {
+        if (bulanLahir > 0) {
+            if (bulanLahir < 13) {
+                this.bulanLahir = bulanLahir;
+            } else {
+                throw new Exception("Bulan Salah ===> Lebih dari 12");
+            }
+        } else {
+            throw new Exception("Bulan Salah ===> Kurang dari 0");
+        }
+
+    }
+
+    public int getTahunLahir() {
+        return tahunLahir;
+    }
+
+    public void setTahunLahir(int tahunLahir) throws Exception {
+        if (tahunLahir > 0) {
+            this.tahunLahir = tahunLahir;
+        } else {
+            throw new Exception("Tahun Salah");
+        }
+    }
+
+    public String buatNomorRekamMedis() {
+        String nomorRekamMedis;
+        Date date = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("yyyMMdd");
+        nomorRekamMedis = ft.format(date) + nama.substring(0, 3);
+        return nomorRekamMedis;
+    }
+}
---
+++ src/Test/TestPasienTry.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import Poliklinik.Dokter;
+import Poliklinik.Pasien;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Martin Paramarta
+ */
+public class TestPasienTry {
+
+    public static void main(String[] args) {
+        Pasien pasien1 = new Pasien("Martin");
+        Dokter dokter1 = new Dokter("Dr. Jono");
+
+        try {
+            pasien1.setAlamat("Klaten");
+            pasien1.setTempatLahir("Klaten");
+            pasien1.setTanggalLahir(3);
+            pasien1.setBulanLahir(11);
+            pasien1.setTahunLahir(1999);
+            pasien1.setNoRekamMedis(pasien1.buatNomorRekamMedis());
+            dokter1.setNoPegawai("696969");
+            dokter1.setAlamat("Sleman");
+            dokter1.setTempatLahir("Sorong");
+            dokter1.setTanggalLahir(16);
+            dokter1.setBulanLahir(10);
+            dokter1.setTahunLahir(1980);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("===========================================");
+        System.out.println("PASIEN");
+        System.out.println("===========================================");
+        System.out.println("Nama            : " + pasien1.getNama());
+        System.out.println("Alamat          : " + pasien1.getAlamat());
+        System.out.println("Tempat Lahir    : " + pasien1.getTempatLahir());
+        System.out.println("Tanggal Lahir   : " + pasien1.getTanggalLahir() + "/" + pasien1.getBulanLahir() + "/" + pasien1.getTahunLahir());
+        System.out.println("No Rekam Medis  : " + pasien1.getNoRekamMedis().toUpperCase());
+        System.out.println("");
+
+        System.out.println("===========================================");
+        System.out.println("DOKTER");
+        System.out.println("===========================================");
+        System.out.println("Nama            : " + dokter1.getNama());
+
+        System.out.println("Alamat          : " + dokter1.getAlamat());
+        System.out.println("Tempat Lahir    : " + dokter1.getTempatLahir());
+        System.out.println("Tanggal Lahir   : " + dokter1.getTanggalLahir() + "/" + dokter1.getBulanLahir() + "/" + dokter1.getTahunLahir());
+        System.out.println("No Pegawai      : " + dokter1.getNoPegawai());
+    }
+}
